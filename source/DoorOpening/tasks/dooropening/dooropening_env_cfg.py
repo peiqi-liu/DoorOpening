80139e52f65985a44b9c4b7f91929a2c72a541cd
Peiqi Liu <leo20021210@163.com>
2026-01-29 15:39:08 -0500

    update

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -123,17 +123,17 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
     robot_body_quat_w = 0.0
     robot_key_body_pos_w = 2.0
     robot_base_joint_pos_w = 2.0
-    robot_arm_joint_pos_w = 5.0
+    robot_arm_joint_pos_w = 3.0
     robot_finger_joint_pos_w = 1.0
-    robot_base_joint_vel_w = 1.0
-    robot_arm_joint_vel_w = 2.0
+    robot_base_joint_vel_w = 0.5
+    robot_arm_joint_vel_w = 1.0
     robot_finger_joint_vel_w = 0.5
-    door_joint_pos_w = 4.0
+    door_joint_pos_w = 5.0
 
     robot_body_quat_scale = 1.0
     robot_key_body_pos_scale = 3.0
     robot_base_joint_pos_scale = 0.5
-    robot_arm_joint_pos_scale = 0.75
+    robot_arm_joint_pos_scale = 0.2
     robot_finger_joint_pos_scale = 1.0
     robot_base_joint_vel_scale = 0.5
     robot_arm_joint_vel_scale = 0.5
