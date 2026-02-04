ac0b58d176d19124c90e04919a495bdbd01701df
Peiqi Liu <leo20021210@163.com>
2026-02-03 23:28:39 -0500

    add more bodies

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -131,7 +131,7 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
 
     door_joint_names = ["joint_1", "joint_2"]
 
-    robot_key_bodies = ["tidybot2_base_link", "panda_link2", "panda_link4", "palm_center"]
+    robot_key_bodies = ["tidybot2_base_link", "panda_link2", "panda_link4", "panda_link6", "panda_link7", "palm_lower", "palm_center"]
     robot_reset_key_bodies = ["tidybot2_base_link", "palm_center"]
 
     robot_palm_link_name = "palm_center"
