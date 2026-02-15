2584a30ca4477f4bd045bda57a31dc1c3c032206
Peiqi Liu <leo20021210@163.com>
2026-02-14 23:56:00 -0500

    update reset hyperparameters

--- source/DoorOpening/tasks/dooropening/dooropening_env.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env.py
@@ -208,9 +208,9 @@ class DooropeningEnv(DirectRLEnv):
         # contact_forces_door2 = contact_forces_door2.reshape(self.num_envs, 1, -1)
         # contact_forces_robot_palm_center = contact_forces_robot_palm_center.reshape(self.num_envs, 1, -1)
 
-        phase = (self.ref_motion_lib.frame_idx % self.ref_motion_lib.num_frames) \
-            / self.ref_motion_lib.num_frames
-        # frame_idx = torch.ceil(self.ref_motion_lib.frame_idx).unsqueeze(dim = -1).to(self.device) // (self.ref_motion_lib.num_frames // 10)
+        # phase = (self.ref_motion_lib.frame_idx % self.ref_motion_lib.num_frames) \
+        #     / self.ref_motion_lib.num_frames
+        frame_idx = torch.ceil(self.ref_motion_lib.frame_idx).unsqueeze(dim = -1).to(self.device) // (self.ref_motion_lib.num_frames // 10)
 
         # ref_joint_vel = self.ref_joint_vel[:, self.ref_robot_dof_idx]
 
@@ -224,8 +224,8 @@ class DooropeningEnv(DirectRLEnv):
                 # ref_joint_vel.unsqueeze(dim = 1),
                 self.door_joint_pos[:, self._door_joint_idx].unsqueeze(dim = 1),
                 door_joint_err.unsqueeze(dim = 1),
-                phase.unsqueeze(dim = -1).unsqueeze(dim = -1),
-                # frame_idx.unsqueeze(dim = -1),
+                # phase.unsqueeze(dim = -1).unsqueeze(dim = -1),
+                frame_idx.unsqueeze(dim = -1),
                 # contact_forces_door1,
                 # contact_forces_door2,
                 # contact_forces_robot_palm_center,
