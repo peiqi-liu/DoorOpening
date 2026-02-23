ebb6d00129e6b2bb6ff2c8d7ecb4f1afff6789cd
Peiqi Liu <leo20021210@163.com>
2026-02-22 21:32:14 -0500

    stricter reset

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -186,13 +186,13 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
     robot_finger_joint_vel_scale = 0.5
     door_joint_pos_scale = 5.0
 
-    reset_base_pos_delta_min = 0.2
-    reset_key_body_pos_delta_min = 0.4
-    reset_key_body_quat_delta_min = 0.4
-    reset_base_pos_delta_max = 0.8
-    reset_key_body_pos_delta_max = 1.0
-    reset_key_body_quat_delta_max = 1.5
-    reset_door_joint_pos_delta_min = 0.4
+    reset_base_pos_delta_min = 0.35
+    reset_key_body_pos_delta_min = 0.6
+    reset_key_body_quat_delta_min = 0.8
+    reset_base_pos_delta_max = 0.5
+    reset_key_body_pos_delta_max = 0.8
+    reset_key_body_quat_delta_max = 1.0
+    reset_door_joint_pos_delta_min = 0.5
     reset_door_joint_pos_delta_max = 0.8
     # We are slowly increasing our tolerance on base position drift and slowly only resettting the env from the first key frame
     # This variable is used to indicate when we stop increasing the tolerance and reset the env from the first key frame for the greatest probability
