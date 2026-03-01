077e8a053a26f974c1b1c276eaaacfb680a6ed53
Peiqi Liu <leo20021210@163.com>
2026-03-01 02:01:55 -0500

    fix bug

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -196,7 +196,7 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
         len(door_body_names) * 3 +\
         len(robot_key_bodies) * 3 * 2 +\
         len(door_joint_names) * 2 +\
-        len(twist_indices) * (len(robot_key_bodies) * 3 + len(robot_key_bodies) * 4 + 3 + len(door_joint_names)) +\
+        len(twist_indices) * (len(robot_key_bodies) * 3 + len(robot_key_bodies) * 3 + 3 + len(door_joint_names)) +\
         actuated_joints_num
     #  5 * 3 +\
     
