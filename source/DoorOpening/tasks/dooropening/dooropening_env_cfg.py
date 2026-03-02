87e6f81f88bb4247b555f2ef95f921f0ae6898c8
Peiqi Liu <leo20021210@163.com>
2026-03-01 22:52:13 -0500

    reduce finger joints

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -73,23 +73,35 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
         'panda_joint7',
     ]
 
+    # finger_joints = [
+    #     'finger_joint_0',
+    #     'finger_joint_1',
+    #     'finger_joint_2',
+    #     'finger_joint_3',
+    #     'finger_joint_4',
+    #     'finger_joint_5',
+    #     'finger_joint_6',
+    #     'finger_joint_7',
+    #     'finger_joint_8',
+    #     'finger_joint_9',
+    #     'finger_joint_10',
+    #     'finger_joint_11',
+    #     'finger_joint_12',
+    #     'finger_joint_13',
+    #     'finger_joint_14',
+    #     'finger_joint_15',
+    # ]
+
     finger_joints = [
-        'finger_joint_0',
         'finger_joint_1',
         'finger_joint_2',
         'finger_joint_3',
-        'finger_joint_4',
         'finger_joint_5',
         'finger_joint_6',
         'finger_joint_7',
-        'finger_joint_8',
         'finger_joint_9',
         'finger_joint_10',
         'finger_joint_11',
-        'finger_joint_12',
-        'finger_joint_13',
-        'finger_joint_14',
-        'finger_joint_15',
     ]
 
     abduction_joints = [
