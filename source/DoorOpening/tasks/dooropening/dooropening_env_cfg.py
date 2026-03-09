283885a07f400ac6ce230b51f9bce292fdb6a1c5
Peiqi Liu <leo20021210@163.com>
2026-03-09 16:28:12 -0400

    fast training

--- source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env_cfg.py
@@ -190,7 +190,7 @@ class DooropeningEnvCfg(DirectRLEnvCfg):
         ),
     )
 
-    twist_indices = [1, 5, 10, 20, 30, 40]
+    twist_indices = [1, 5, 20]
 
     # door(s)
     door_cfg: ArticulationCfg = ALL_DOOR_CONFIGS.replace(prim_path="/World/envs/env_.*/Door")
