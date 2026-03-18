8c616827e9a777322f7191c734d6f583937dae87
Peiqi Liu <leo20021210@163.com>
2026-03-18 16:09:51 -0400

    minor update

--- scripts/distillation/run_distillation.py
+++ scripts/distillation/run_distillation.py
@@ -39,7 +39,7 @@ def _load_student_dagger_defaults(student_cfg_path):
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with RL-Games.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
-parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+parser.add_argument("--video_length", type=int, default=1000, help="Length of the recorded video (in steps).")
 parser.add_argument("--video_interval", type=int, default=5000, help="Interval between video recordings (in steps).")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
