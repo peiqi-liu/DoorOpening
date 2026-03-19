f07c1c9b64aa4e4858f27996199d9bf0cd6f236d
Peiqi Liu <leo20021210@163.com>
2026-03-19 00:19:07 -0400

    add pointnet

--- source/DoorOpening/tasks/dooropening/dooropening_env.py
+++ source/DoorOpening/tasks/dooropening/dooropening_env.py
@@ -128,7 +128,7 @@ class DooropeningEnv(DirectRLEnv):
         self.handle_offsets = torch.stack(self.handle_offsets).to(self.device)
         self.board_offsets = torch.stack(self.board_offsets).to(self.device)
         env_to_file_map = [i % len(motion_traj_paths) for i in range(self.num_envs)]
-        self.ref_motion_lib = ReferenceMotionManager(num_envs=self.num_envs, device=self.device, velocity=self.cfg.velocity, reset_from_start = True, env_to_file_map=env_to_file_map, twist_indices=self.twist_indices)
+        self.ref_motion_lib = ReferenceMotionManager(num_envs=self.num_envs, device=self.device, velocity=self.cfg.velocity, reset_from_start = False, env_to_file_map=env_to_file_map, twist_indices=self.twist_indices)
         self.max_trial_steps = self.ref_motion_lib.num_frames * torch.ones_like(self.episode_length_buf, device=self.device)
 
         torch.set_printoptions(precision=4, sci_mode=False)
